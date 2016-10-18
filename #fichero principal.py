919e54abffaf65dae47edd468abbb89306025cbc
Ander Eguiluz <ander.eguiluz@opendeusto.es>
2016-10-18 13:39:47 +0200

    He añadido mi primer fichero

---
+++ #fichero principal.py
@@ -0,0 +1,15 @@
+#fichero principal
+
+def main ( ):
+if__name__== '__main__' :
+	main( )
+
+
+import funcionesprueba 
+
+suma = funcionesprueba.sumar(3,5)
+
+print(suma)
+
+funcionesprueba.saludar("Ander")
+
